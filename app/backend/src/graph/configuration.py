7cdbe77b44faef1afb8ef9080e12913e56f603a0
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-12-30 23:10:53 +0530

    feat: Add retrieval tool to retrieve docs from vectorstore

--- app/backend/src/graph/configuration.py
+++ app/backend/src/graph/configuration.py
@@ -7,13 +7,15 @@ class Settings(BaseSettings):
     """Application settings and environment variables."""
 
     model: str = "gpt-4o"
-    embedding_model: str = "text-embedding-3-large"
     temperature: float = 0.0
 
     openai_api_key: str
 
     database_url: str
     database_sslmode: str = "disable"
+
+    embedding_model: str = "text-embedding-3-large"
+    top_k_docs: int = 5
 
     class Config:
         env_file = ".env"
---
+++ app/backend/src/graph/tool.py
@@ -0,0 +1,35 @@
+from langchain.tools.retriever import create_retriever_tool
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres.vectorstores import PGVector
+
+from .configuration import get_settings
+
+
+def setup_vector_store() -> PGVector:
+    """Initialize and return the vector store."""
+    settings = get_settings()
+
+    return PGVector(
+        embeddings=OpenAIEmbeddings(model=settings.embedding_model),
+        collection_name="nextjs_docs",
+        connection=settings.database_url + "?sslmode=" + settings.database_sslmode,
+        use_jsonb=True,
+    )
+
+
+def create_retrieval_tool() -> dict:
+    """Create a retrieval tool with the given retriever."""
+    settings = get_settings()
+    vector_store = setup_vector_store()
+    retriever = vector_store.as_retriever(
+        search_kwargs={"k": settings.top_k_docs},
+    )
+    return create_retriever_tool(
+        retriever,
+        "retrieve_documentation",
+        "Search and retrieve relevant documentation based on the query. Returns documentation snippets that best match the question.",
+    )
+
+
+retrieval_tool = create_retrieval_tool()
+TOOLS = [retrieval_tool]
