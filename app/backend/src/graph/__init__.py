586f4fae9c72aa03192ffbbc175611fffc6df14f
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-12-30 08:51:08 +0530

    feat: Add a simple React Agent

--- app/backend/src/graph/__init__.py
+++ app/backend/src/graph/__init__.py
@@ -1,2 +1,244 @@
+import os
+import pprint
+from typing import Literal
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.tools.retriever import create_retriever_tool
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_postgres.vectorstores import PGVector
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+
 def main() -> None:
-    print("Hello from backend!")
+    connection_string = os.getenv("DATABASE_URL")
+    if not connection_string:
+        raise ValueError("DATABASE_URL environment variable is not set")
+
+    embedding_model = os.getenv("EMBEDDING_MODEL")
+    if not embedding_model:
+        raise ValueError("EMBEDDING_MODEL environment variable is not set")
+
+    collection_name = "nextjs_docs"
+    vector_store = PGVector(
+        embeddings=OpenAIEmbeddings(model=embedding_model),
+        collection_name=collection_name,
+        connection=connection_string,
+        use_jsonb=True,
+    )
+    retriever = vector_store.as_retriever()
+
+    retriever_tool = create_retriever_tool(
+        retriever,
+        "retrieve_docs",
+        "Search and return information about Next.js docs.",
+    )
+
+    tools = [retriever_tool]
+
+    def grade_documents(state) -> Literal["generate", "rewrite"]:
+        """
+        Determines whether the retrieved documents are relevant to the question.
+
+        Args:
+            state (messages): The current state
+
+        Returns:
+            str: A decision for whether the documents are relevant or not
+        """
+
+        print("---CHECK RELEVANCE---")
+
+        # Data model
+        class grade(BaseModel):
+            """Binary score for relevance check."""
+
+            binary_score: str = Field(description="Relevance score 'yes' or 'no'")
+
+        # LLM
+        model = ChatOpenAI(temperature=0, model="gpt-4-0125-preview", streaming=True)
+
+        # LLM with tool and validation
+        llm_with_tool = model.with_structured_output(grade)
+
+        # Prompt
+        prompt = PromptTemplate(
+            template="""You are a grader assessing relevance of a retrieved document to a user question. \n 
+            Here is the retrieved document: \n\n {context} \n\n
+            Here is the user question: {question} \n
+            If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+            Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
+            input_variables=["context", "question"],
+        )
+
+        # Chain
+        chain = prompt | llm_with_tool
+
+        messages = state["messages"]
+        last_message = messages[-1]
+
+        question = messages[0].content
+        docs = last_message.content
+
+        scored_result = chain.invoke({"question": question, "context": docs})
+
+        score = scored_result.binary_score
+
+        if score == "yes":
+            print("---DECISION: DOCS RELEVANT---")
+            return "generate"
+
+        else:
+            print("---DECISION: DOCS NOT RELEVANT---")
+            print(score)
+            return "rewrite"
+
+    def agent(state):
+        """
+        Invokes the agent model to generate a response based on the current state. Given
+        the question, it will decide to retrieve using the retriever tool, or simply end.
+
+        Args:
+            state (messages): The current state
+
+        Returns:
+            dict: The updated state with the agent response appended to messages
+        """
+        print("---CALL AGENT---")
+        messages = state["messages"]
+        model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4-turbo")
+        model = model.bind_tools(tools)
+        response = model.invoke(messages)
+        # We return a list, because this will get added to the existing list
+        return {"messages": [response]}
+
+    def rewrite(state):
+        """
+        Transform the query to produce a better question.
+
+        Args:
+            state (messages): The current state
+
+        Returns:
+            dict: The updated state with re-phrased question
+        """
+
+        print("---TRANSFORM QUERY---")
+        messages = state["messages"]
+        question = messages[0].content
+
+        msg = [
+            HumanMessage(
+                content=f""" \n 
+        Look at the input and try to reason about the underlying semantic intent / meaning. \n 
+        Here is the initial question:
+        \n ------- \n
+        {question} 
+        \n ------- \n
+        Formulate an improved question: """,
+            )
+        ]
+
+        # Grader
+        model = ChatOpenAI(temperature=0, model="gpt-4-0125-preview", streaming=True)
+        response = model.invoke(msg)
+        return {"messages": [response]}
+
+    def generate(state):
+        """
+        Generate answer
+
+        Args:
+            state (messages): The current state
+
+        Returns:
+            dict: The updated state with re-phrased question
+        """
+        print("---GENERATE---")
+        messages = state["messages"]
+        question = messages[0].content
+        last_message = messages[-1]
+
+        docs = last_message.content
+
+        # Prompt
+        prompt = hub.pull("rlm/rag-prompt")
+
+        # LLM
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
+
+        # Post-processing
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
+
+        # Chain
+        rag_chain = prompt | llm | StrOutputParser()
+
+        # Run
+        response = rag_chain.invoke({"context": docs, "question": question})
+        return {"messages": [response]}
+
+    print("*" * 20 + "Prompt[rlm/rag-prompt]" + "*" * 20)
+    prompt = hub.pull(
+        "rlm/rag-prompt"
+    ).pretty_print()  # Show what the prompt looks like
+
+    # Define a new graph
+    workflow = StateGraph(MessagesState)
+
+    # Define the nodes we will cycle between
+    workflow.add_node("agent", agent)  # agent
+    retrieve = ToolNode([retriever_tool])
+    workflow.add_node("retrieve", retrieve)  # retrieval
+    workflow.add_node("rewrite", rewrite)  # Re-writing the question
+    workflow.add_node(
+        "generate", generate
+    )  # Generating a response after we know the documents are relevant
+    # Call agent node to decide to retrieve or not
+    workflow.add_edge(START, "agent")
+
+    # Decide whether to retrieve
+    workflow.add_conditional_edges(
+        "agent",
+        # Assess agent decision
+        tools_condition,
+        {
+            # Translate the condition outputs to nodes in our graph
+            "tools": "retrieve",
+            END: END,
+        },
+    )
+
+    # Edges taken after the `action` node is called.
+    workflow.add_conditional_edges(
+        "retrieve",
+        # Assess agent decision
+        grade_documents,
+    )
+    workflow.add_edge("generate", END)
+    workflow.add_edge("rewrite", "agent")
+
+    # Compile
+    graph = workflow.compile()
+
+    inputs = {
+        "messages": [
+            (
+                "user",
+                "what is the folder structure of a next.js project with app router vs page router?",
+            ),
+        ]
+    }
+    for output in graph.stream(inputs):
+        for key, value in output.items():
+            pprint.pprint(f"Output from node '{key}':")
+            pprint.pprint("---")
+            pprint.pprint(value, indent=2, width=80, depth=None)
+        pprint.pprint("\n---\n")
